b0c8c624326ce36618e8f9946779f1726648fdb3
sakran <sakran.saleh19@gmail.com>
2019-05-22 08:53:29 +0600

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is awesome...");
+
+        greeting("Sakran Saleh");
+    }
+
+
+    public static void greeting(String name){
+        System.out.println("Welcome " + name);
     }
 }
